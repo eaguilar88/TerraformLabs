5adf010c2a62053a406a89c1eb448d8067b48f97
Ellery Aguilar <esaulagj@gmail.com>
2026-03-06 18:06:52 -0600

    test-semana-1. usar resultado y concatenar strings

--- proveedores/main.tf
+++ proveedores/main.tf
@@ -1,5 +1,5 @@
 resource "local_file" "password" {
-  content              = "nolodire"
+  content              = "Password: ${random_string.iac_random.result}"
   filename             = "password.txt"
   file_permission      = 0400
   directory_permission = 0600
