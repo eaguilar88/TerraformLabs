975430f6c118550f9d10d5f6483c5fbcc3d67958
Ubuntu <azureuser@kube02.3iialrjxvjnupgyqymfqqlyuna.cx.internal.cloudapp.net>
2026-03-27 04:05:13 +0000

    adjust main file

--- PracticaFinal/main.tf
+++ PracticaFinal/main.tf
@@ -2,7 +2,7 @@ module "network-module" {
   source      = "./network"
   vpc_cidr    = lookup(var.vpc_cidr, terraform.workspace)
   subnet_cidr = lookup(var.subnet_cidr, terraform.workspace)
-  tags        = var.tags["owner"]
+  tags        = var.tags
 }
 
 
@@ -11,5 +11,5 @@ module "infra-module" {
   ami           = lookup(var.ami, terraform.workspace)
   subnet_id     = module.network-module.out-subnet-id
   instance_type = lookup(var.instancia, terraform.workspace)
-  tags          = var.tags["owner"]
+  tags          = var.tags
 }
